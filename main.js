0665260f49916422095c9cfde05c845f47dab82c
puneetGupts <puneetgupta899@gmail.com>
2022-02-22 11:31:51 -0600

    Adding logo

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+//Login Validation Email and password
+// var email = document.getElementById("email");
+// var span1 = document.createElement("span");	
+
+// var pwd = document.getElementById("pwd");
+// var span2 = document.createElement("span");
+
+
+// email.onfocus = function(){
+//   span1.innerHTML = "Email is incorrect";
+//   span1.style.display = "flex";
+//   email.appendChild(span1);
+//   console.log(email.parentNode.firstElementChild);
+
+//   email.classList.remove('error');
+// }
+
+// email.onblur = function(){
+//   span1.style.display = "none";
+//   email.parentNode.appendChild(span1);
+// }
+
+// pwd.onfocus = function(){
+//   span2.innerHTML = "Password is incorrect";
+//   span2.style.display = "flex";
+//   pwd.parentNode.appendChild(span2);
+//   pwd.classList.remove('error');
+// }
+
+// pwd.onblur = function(){
+//   span2.style.display = "none";
+//   pwd.parentNode.appendChild(span2);
+// }
+
+
 // popover
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
