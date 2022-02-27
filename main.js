2486a7e112a1efdfbc8506605c399d83a8de2223
NiraliIncap <npatel@incapsulate.com>
2022-02-27 16:15:52 -0600

    Updated registration code based on Teams call

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 });
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 // Gender Select
 if (window.location.pathname === "/") {
